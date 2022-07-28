35937e5fabd0c485cd758d829e0be6f7d8bea0aa
ibnbayo <abdullahifahm@gmail.com>
2022-07-29 00:42:13 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import logo from './assets/logo.svg';
+
+
+function Header() {
+    return(
+        <div>
+            <img src={logo} alt="logo" />
+            
+
+        </div>
+    )
+}
+export default Header
