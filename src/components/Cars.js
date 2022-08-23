6da2fbdab1150ed012fe6b6b91c7d90699e1d3cd
ibnbayo <abdullahifahm@gmail.com>
2022-08-23 06:00:17 +0100

    render

--- src/components/Cars.js
+++ src/components/Cars.js
@@ -1,20 +1,44 @@
 import {useState, useEffect} from "react"
 import axios from "axios"
+import './Cars.css'
+
+const options = {
+    method: 'GET',
+    url: 'https://all-cars.p.rapidapi.com/cars/mazda',
+    headers: {
+      'X-RapidAPI-Key': 'c0d2e70417msh3bde3b7dbe9e25ap12748ejsncd1fe394742c',
+      'X-RapidAPI-Host': 'all-cars.p.rapidapi.com'
+    }
+  }
 
 function Cars() {
+    const [carz, setCarz] = useState([])
 
     useEffect(() => {
-        fetch("https://my.api.mockaroo.com/cars.json?key=9b52d4c0")
-        .then(response => response.json())
-        .then(carsInfo => setAllCars(carsInfo))
+        axios.request(options)
+        .then(response => {
+            setCarz([...response.data])
+            console.log(response.data)
 
+        }).catch(function (error) {
+            console.error(error);
+        })
     },[])
+    console.log(carz)
 
-    const [allCars, setAllCars] = useState([])
-    console.log(allCars)
+
+    // const [allCars, setAllCars] = useState([])
+    // console.log(allCars)
     return(
-        <div>
-
+        <div className="cars">
+            {carz.map((car) => (
+                <div className="car-wrap" key={carz.indexOf(car)}>
+                    <img className="car-img" src={`${car.img}`}/>
+                    <div className="car-title">{car.title}</div>
+                </div>
+            ))}
+            
+    
         </div>
     )
 }
